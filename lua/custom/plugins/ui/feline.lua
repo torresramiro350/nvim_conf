ecff9a1a1a3338e5005949982bbc1e93288ae0a1
Ramiro Torres-Escobedo <rtorrese@proton.me>
2024-04-12 15:19:24 +0800

    minor changes to feline and lualine config file

--- lua/custom/plugins/ui/feline.lua
+++ lua/custom/plugins/ui/feline.lua
@@ -1,28 +1,30 @@
 return {
-	"freddiehaddad/feline.nvim",
-	config = function(_, opts)
-		-- SIMPLER SETUP
-		local ctp_feline = require("catppuccin.groups.integrations.feline")
+  "freddiehaddad/feline.nvim",
+  -- config = function(_, opts)
+  config = function()
+    -- SIMPLER SETUP
+    local ctp_feline = require("catppuccin.groups.integrations.feline")
 
-		ctp_feline.setup({
-			--- default setup
-			-- inactive = {
-			-- 	"file_info",
-			-- },
-			view = {
-				lsp = {
-					progress = true,
-					name = true,
-					separator = "|",
-					exclude_lsp_names = { "null-ls", "copilot" },
-				},
-			},
-		})
+    ctp_feline.setup({
+      --- default setup
+      inactive = {
+        -- "file_info",
+      },
+      view = {
+        lsp = {
+          progress = true,
+          name = false,
+          separator = "|",
+          exclude_lsp_names = { "null-ls", "copilot" },
+        },
+      },
+    })
 
-		require("feline").setup({
-			components = ctp_feline.get(),
-		})
-	end,
-	priority = 1000,
-	event = "UIEnter",
+    require("feline").setup({
+      components = ctp_feline.get(),
+    })
+  end,
+  -- priority = 1000,
+  after = "catppuccin",
+  event = "UIEnter",
 }
