71bc3530c3b0fa2f7dd4d5ad9acf8a32719b16a5
Ramiro Torres-Escobedo <rtorrese@proton.me>
2024-04-21 23:09:14 +0800

    updating some plugins configurations

--- lua/custom/plugins/completion/nvim_cmp.lua
+++ lua/custom/plugins/completion/nvim_cmp.lua
@@ -91,7 +91,7 @@ return {
           -- require("copilot_cmp.comparators").prioritize,
           cmp.config.compare.offset,
           cmp.config.compare.recently_used,
-          require("clangd_extensions.cmp_scores"),
+          -- require("clangd_extensions.cmp_scores"),
           cmp.config.compare.kind,
           cmp.config.compare.sort_text,
           cmp.config.compare.length,
--- lua/custom/plugins/rust/rust_tools.lua
+++ lua/custom/plugins/rust/rust_tools.lua
@@ -1,6 +1,7 @@
 return {
 
-  "mrcjkb/rustaceanvim",
-  version = "^4", -- Recommended
-  ft = { "rust" },
+	"mrcjkb/rustaceanvim",
+	event = "VeryLazy",
+	version = "^4", -- Recommended
+	ft = { "rust" },
 }
