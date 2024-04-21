7cc746edc474610b818eedfa29a25ebba685fcc4
Ramiro Torres-Escobedo <rtorrese@proton.me>
2024-04-21 23:21:04 +0800

    cleaning some unused plugins

--- lua/lazyvim/init.lua
+++ lua/lazyvim/init.lua
@@ -3,27 +3,27 @@
 --    `:help lazy.nvim.txt` for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-	{ import = "custom.plugins" },
-	{ import = "custom.plugins.completion" },
-	{ import = "custom.plugins.cpp" },
-	{ import = "custom.plugins.documentation" },
-	{ import = "custom.plugins.enhancements" },
-	{ import = "custom.plugins.git" },
-	{ import = "custom.plugins.lsp" },
-	{ import = "custom.plugins.rust" },
-	{ import = "custom.plugins.treesitter" },
-	{ import = "custom.plugins.telescope" },
-	{ import = "custom.plugins.tmux" },
-	{ import = "custom.plugins.ui" },
+  -- { import = "custom.plugins" },
+  { import = "custom.plugins.completion" },
+  { import = "custom.plugins.cpp" },
+  { import = "custom.plugins.documentation" },
+  { import = "custom.plugins.enhancements" },
+  { import = "custom.plugins.git" },
+  { import = "custom.plugins.lsp" },
+  { import = "custom.plugins.rust" },
+  { import = "custom.plugins.treesitter" },
+  { import = "custom.plugins.telescope" },
+  { import = "custom.plugins.tmux" },
+  { import = "custom.plugins.ui" },
 })
