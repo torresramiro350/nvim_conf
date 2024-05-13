8d68569d30b0b26561ff80258600e2246784a509
Ramiro Torres-Escobedo <rtorrese@proton.me>
2024-05-13 15:08:42 +0800

    moved some of the shortcuts from neogen's configuration to the mapping

--- lua/custom/plugins/documentation/neogen_lua.lua
+++ lua/custom/plugins/documentation/neogen_lua.lua
@@ -1,45 +1,41 @@
 return {
-	"danymat/neogen",
-	config = function()
-		-- adding some keybindings
-		-- local opts = { noremap = true, silent = true }
-		vim.keymap.set("n", "<leader>nf", "<cmd>Neogen func<cr>", { desc = "Generate func docs " })
-		vim.keymap.set("n", "<leader>nc", "<cmd>Neogen class<cr>", { desc = "Generate class docs " })
-		vim.keymap.set("n", "<leader>nF", "<cmd>Neogen file<cr>", { desc = "Generate file docs " })
-
-		require("neogen").setup({
-			enabled = true,
-			input_after_comment = true,
-			languages = {
-				lua = {
-					template = {
-						annotation_convention = "emmylua",
-					},
-				},
-				python = {
-					template = {
-						annotation_convention = "reST",
-						-- annotation_convention = "numpydoc",
-					},
-				},
-				cpp = {
-					template = {
-						annotation_convention = "doxygen",
-					},
-				},
-				rust = {
-					template = {
-						annotation_convention = "rustdoc",
-					},
-				},
-				sh = {
-					template = {
-						annotation_convention = "google_bash",
-					},
-				},
-			},
-		})
-	end,
-	dependencies = { "nvim-treesitter/nvim-treesitter" },
-	event = { "BufReadPost", "InsertEnter" },
+  "danymat/neogen",
+  config = function()
+    -- adding some keybindings
+    -- local opts = { noremap = true, silent = true }
+    require("neogen").setup({
+      enabled = true,
+      input_after_comment = true,
+      languages = {
+        lua = {
+          template = {
+            annotation_convention = "emmylua",
+          },
+        },
+        python = {
+          template = {
+            -- annotation_convention = "reST",
+            annotation_convention = "numpydoc",
+          },
+        },
+        cpp = {
+          template = {
+            annotation_convention = "doxygen",
+          },
+        },
+        rust = {
+          template = {
+            annotation_convention = "rustdoc",
+          },
+        },
+        sh = {
+          template = {
+            annotation_convention = "google_bash",
+          },
+        },
+      },
+    })
+  end,
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  event = { "BufReadPost", "InsertEnter" },
 }
