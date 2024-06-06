b0603ba442f54027348592a6d3085d7d3ba29335
Ramiro Torres-Escobedo <rtorrese@proton.me>
2024-06-06 10:37:11 +0800

    add rainbow coloring to load when editing a new file

--- lua/plugins/ui/rainbow.lua
+++ lua/plugins/ui/rainbow.lua
@@ -1,10 +1,10 @@
 return {
-	"HiPhish/rainbow-delimiters.nvim",
-	config = function()
-		require("rainbow-delimiters.setup").setup({
-			-- call options here
-		})
-	end,
-	-- when starting to edit a new buffer
-	event = { "BufReadPost", "BufReadPre" },
+  "HiPhish/rainbow-delimiters.nvim",
+  config = function()
+    require("rainbow-delimiters.setup").setup({
+      -- call options here
+    })
+  end,
+  -- when starting to edit a new buffer
+  event = { "BufNewFile", "BufReadPost", "BufReadPre" },
 }
